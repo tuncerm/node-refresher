04230504ee51b8c579259ade43ce16a33af944c1
Mustafa TUNCER <tuncerm@gmail.com>
2020-03-05 00:26:23 +0100

    First req/res

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const fs = require('fs');
+const http = require('http');
 
-const userName = 'Havoc';
-
-fs.writeFile(`${userName}-data.txt`, 'Name: ' + userName, (err) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log('WROTE FILE');
+const server = http.createServer((req, res)=>{
+  console.log('INCOMING...');
+  console.log(req.method, req.url);
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Success!</h1>');
 });
+
+server.listen(5000);
