9fed1e1a416b387a6c3034ccd881bdb010e0ae0e
Mustafa TUNCER <tuncerm@gmail.com>
2020-03-05 00:35:16 +0100

    handling get/post requests natively.

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-  console.log('INCOMING...');
-  console.log(req.method, req.url);
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Success!</h1>');
+  if(req.method === 'POST') {
+    let body = '';
+    req.on('end', ()=>{
+      console.log(body);
+      res.end(`<h1>Got the post request ${body}<h1>`)
+    });
+    req.on('data', (chunk)=>{
+      body += chunk;
+    });
+  } else {
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`
+    <form method="POST">
+      <input type="text" name="username">
+      <button type="submit">Create User</button>
+    </form>
+  `);
+  }
 });
 
 server.listen(5000);
