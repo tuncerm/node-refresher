07e90b680f3bb4cd90bf4b2724749fc84bfd695c
Mustafa TUNCER <tuncerm@gmail.com>
2020-03-05 00:52:06 +0100

    handling request with express the manual way...

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res)=>{
-  if(req.method === 'POST') {
-    let body = '';
-    req.on('end', ()=>{
-      console.log(body);
-      res.end(`<h1>Got the post request ${body}<h1>`)
-    });
-    req.on('data', (chunk)=>{
-      body += chunk;
-    });
-  } else {
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`
-    <form method="POST">
-      <input type="text" name="username">
-      <button type="submit">Create User</button>
-    </form>
-  `);
-  }
+app.use((req, res, next)=>{
+  let body = '';
+  req.on('end', ()=>{
+    const [key, value] = body.split('=');
+    req.body = {name:value};
+    next();
+  })
+  req.on('data',(chunk)=>{
+    body += chunk;
+  })
 });
 
-server.listen(5000);
+app.use((req, res, next)=>{
+  if(req.body.name){
+    return res.send('<h1>'+ req.body.name +'</h1>');
+  }
+  res.send(`
+  <form method="POST">
+    <input type="text" name="username">
+    <button type="submit">Create User</button>
+  </form>
+`)
+});
+
+
+app.listen(5000);
